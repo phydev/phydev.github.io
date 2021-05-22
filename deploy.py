b588bad6984a280183125ca56599f314de2b427f
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2021-05-22 23:52:45 +0200

    new deploy.py

--- deploy.py
+++ deploy.py
@@ -1,8 +1,25 @@
-import markdown
+import os
 
-with open('lotka.md', 'r') as f:
-    text = f.read()
-    html = markdown.markdown(text)
+extensions = ['html']
+articles = []
+for file in os.listdir('./articles'):
+    if any(x in file for x in extensions):
+        articles.append(file)
 
-with open('lotka.html', 'w') as f:
-    f.write(html)
+
+metadata = {}
+pattern = ''
+for n, article in enumerate(articles):
+    link="./articles/"+article
+    with open(link) as file:
+        line = file.readline()
+        metadata[n] = dict(eval(line[4:-4]))
+    metadata[n]['link'] = link
+
+    if metadata[n]['status'] != 'hidden':
+        pattern += "<p><a href='"+metadata[n]['link']+"'> "+ metadata[n]['title_en']+" </a></p> \n"
+
+
+with open("articles.html", "w") as file:
+    file.write(pattern)
+
