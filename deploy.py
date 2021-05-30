1a4b7dea39c46e0263ae75cdf56bdbc9a815a57b
Maurício <mmoreirauc@gmail.com>
2021-05-31 01:05:47 +0200

    add biocomp

--- deploy.py
+++ deploy.py
@@ -1,25 +1,31 @@
 import os
 
-extensions = ['html']
-articles = []
-for file in os.listdir('./articles'):
-    if any(x in file for x in extensions):
-        articles.append(file)
+def generateIndex(folder, output, extensions = ['html']):
+
+    articles = []
+    for file in os.listdir('./'+folder):
+        if any(x in file for x in extensions):
+            articles.append(file)
 
 
-metadata = {}
-pattern = ''
-for n, article in enumerate(articles):
-    link="/articles/"+article
-    with open("."+link) as file:
-        line = file.readline()
-        metadata[n] = dict(eval(line[4:-4]))
-    metadata[n]['link'] = link
+    metadata = {}
+    pattern = ''
+    for n, article in enumerate(articles):
+        link="/"+folder+"/"+article
+        with open("."+link) as file:
+            line = file.readline()
+            metadata[n] = dict(eval(line[4:-4]))
+        metadata[n]['link'] = link
 
-    if metadata[n]['status'] != 'hidden':
-        pattern += "<p><a href='"+metadata[n]['link']+"'> "+ metadata[n]['title_en']+" </a></p> \n"
+        if metadata[n]['status'] == 'show':
+            pattern += "<p><a href='"+metadata[n]['link']+"'> "+ metadata[n]['title_en']+" </a></p> \n"
 
 
-with open("articles.html", "w") as file:
-    file.write(pattern)
+    with open(output, "w") as file:
+        file.write(pattern)
+
+
+generateIndex(folder="articles", output="articles.html")
+generateIndex(folder="articles/biocomp", output="./articles/biocomp/contents.html")
+
 
