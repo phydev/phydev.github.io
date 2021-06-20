635cdae0c8dbd5ed56d6aaa53ea32638b3cb6e83
Maurício <mmoreirauc@gmail.com>
2021-06-20 23:13:53 +0200

    fix bug in resources deploy

--- deploy.py
+++ deploy.py
@@ -20,8 +20,6 @@ def generateIndex(folder, output, extensions = ['html']):
         metadata[n]['link'] = link
         
         if metadata[n]['status'] == 'show':
-            print(metadata[n]["order"])
-            print("<p><a href='"+metadata[n]['link']+"'> "+ str(metadata[n]["order"])+ " - " + metadata[n]["title"]+" </a></p>")
             patterns[metadata[n]["order"]] = "<p><a href='"+metadata[n]['link']+"'> "+ str(metadata[n]["order"])+ " - " + metadata[n]["title"]+" </a></p>"
             article_number +=1
 
@@ -30,7 +28,7 @@ def generateIndex(folder, output, extensions = ['html']):
             file.write(patterns[str(line)])
 
 
-generateIndex(folder="articles", output="articles.html")
-generateIndex(folder="articles/biocomp", output="./articles/biocomp/contents.html")
+generateIndex(folder="posts", output="posts.html")
+generateIndex(folder="resources/biocomp", output="./resources/contents.html")
 
 
